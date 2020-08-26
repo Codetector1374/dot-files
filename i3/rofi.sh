e0a67bf13e0c687c7db1061ba19da651fea3dbd8
Codetector <codetector@codetector.cn>
2020-08-26 18:33:31 -0400

    i3 rofi
    Signed-off-by: Codetector <codetector@codetector.cn>

--- i3/rofi.sh
+++ i3/rofi.sh
@@ -1,5 +1,6 @@
 bg_color=#2f343f
 text_color=#f3f4f5
 htext_color=#9575cd
+theme="$HOME/dot-files/i3/rofi-theme.cfg"
 
-rofi -show run -lines 3 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -color-window "$bg_color, $bg_color, $bg_color" -color-normal "$bg_color, $text_color, $bg_color, $bg_color, $htext_color" -font "System San Francisco Display 18"
+rofi -show run -theme $theme 
